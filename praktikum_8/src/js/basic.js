2b2ac5d477df0b759dc7bd4042a9a7f41ac15386
donisetiawan12 <ds5042973@gmail.com>
2024-11-23 10:43:03 +0700

    update

--- praktikum_8/src/js/basic.js
+++ praktikum_8/src/js/basic.js
@@ -10,6 +10,31 @@ function hitungLingkaran() {
         innerText = "Luas : " +  luas + "cm², Keliling : " + keliling + "cm²"
     } else {
         document.getElementById("LingkarangOutput").
-        innerText = "Masukan Jari - Jari Yang valid!!1"
+        innerText = "Masukan Jari - Jari Yang valid!!"
     }
+}
+
+//soal 2 
+    function hitungJumlah () {
+        var rupiah = document.getElementById("rupiah").value;
+        if (rupiah > 0){
+            var kurs = 15870;
+            var dollar = rupiah / kurs;
+            document.getElementById("hasilRupiah").innerText = "Nilai : $ " + dollar.toFixed(2);
+        } else {
+            document.getElementById("hasilRupiah").innerHTML = "Masukan Nilai yang Valid!!!";
+        }
+    }
+
+
+//soal 3 
+
+function hitungPenjumahan() {
+    var biL1 = parseFloat(document.getElementById("biL1").value) || 0;
+    var biL2 = parseFloat(document.getElementById("biL2").value) || 0;
+
+    var jumlah = biL1 + biL2;
+    document.getElementById("hasilJumlah").innerText = "Jumlah : " + jumlah.toFixed(2);
+
+    
 }
